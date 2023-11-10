e7e35a3c992b769a5499cb7d67172f809335df25
cj <carlchu0113@gmail.com>
2023-11-10 17:26:02 +0800

    add vite plugin mock

--- vite.config.ts
+++ vite.config.ts
@@ -15,6 +15,8 @@ import UnoCSS from 'unocss/vite'
 
 import { VitePWA } from 'vite-plugin-pwa'
 
+import { viteMockServe } from 'vite-plugin-mock'
+
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [
@@ -53,7 +55,10 @@ export default defineConfig({
       layoutsDirs: 'src/layouts',
       defaultLayout: 'default'
     }),
-    VitePWA({})
+    VitePWA({}),
+    viteMockServe({
+      enable: false
+    })
   ],
   resolve: {
     alias: {
